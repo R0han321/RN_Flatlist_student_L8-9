9f9afec652a0a6c9cfc3a000a295c838d6c2d2a0
R0han321 <113720741+R0han321@users.noreply.github.com>
2025-01-29 08:17:36 -0600

    Finished notes day1

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,16 +7,53 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import { useState } from "react";
 
 export default function Index() {
+
+  const [selectedId, setSelectedID] = useState<string>("1");
+
+  type dataType = {
+    id: string;
+    title: string;
+  }
+
+  const DATA: dataType[] = [
+    {id: "1", title: "First"},
+    {id: "2", title: "Second"},
+    {id: "3", title: "Third"},
+    {id: "4", title: "Fourth"},
+    {id: "5", title: "Fifth"},
+  ];
+
+  const selectedList = (item: dataType) => {
+    setSelectedID(item.id);
+    console.log("selected " + item.title);
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
-        <Text style={defaultStyles.title}>Insert Title Here</Text>
+        <Text style={defaultStyles.title}>FlatList</Text>
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+          data = {DATA}
+          keyExtractor={(item: dataType) => item.id}
+          renderItem={({item}) => (
+            <TouchableOpacity onPress={() => selectedList(item)}>
+              <View style={[styles.titleContainer, 
+                {
+                  backgroundColor: 
+                    item.id === selectedId ? colors.primary : colors.secondary
+                }
+              ]}>
+                <Text style={styles.titleContainer}>{item.title}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          />
         </View>
       </View>
     </View>
@@ -32,6 +69,7 @@ const styles = StyleSheet.create({
     width: 300,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    backgroundColor: "lightblue",
   },
   titleText: {
     fontSize: 24,
